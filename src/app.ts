182aff3f52847819c477a76f0709597e7a099304
KRISH826 <krishnendupanj@gmail.com>
2024-07-25 22:42:27 +0530

    middle wares and error config done

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import createHttpError from "http-errors";
+import ErrorHandler from "./middlewares/errorHandler";
 
 const app = express();
 // routes
 app.get("/", (req, res) => {
+  const error = createHttpError(400, "Not Found");
+  throw error;
   res.send("hi there");
 });
+
+// app.use();
+
+app.use(ErrorHandler);
 
 export default app;
