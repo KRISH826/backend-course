f40a8fb53bf4856b2d7d9d7db136f4543abeaf89
KRISH826 <krishnendupanj@gmail.com>
2024-07-25 19:52:06 +0530

    all works done for this branch

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import app from "./src/app";
 import { config } from "./src/config/config";
+import { connectDb } from "./src/config/db";
 
 /** @format */
-const startServer = () => {
+const startServer = async () => {
   const port = config.port || 3300;
+
+  await connectDb();
 
   app.listen(port, () => {
     console.log(`Starting server: ${port}`);
